c5cc8b0ebe56413895177fc977ab888e4bb4660f
Takayuki Koga <kogatakayuki.icloud.com>
2023-09-02 15:52:35 +0900

    楽天データの取得

---
+++ vook_db_v5/config.py
@@ -0,0 +1,27 @@
+from vook_db_v5.local_config import CLIENT_ME # noqa
+from pathlib import Path
+
+
+REQ_URL = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+MAX_PAGE = 10  # NOTE: auto_postが300ほどしかできない。
+HITS_PER_PAGE = 30
+
+
+req_params = {
+    "applicationId": CLIENT_ME["APPLICATION_ID"],
+    "affiliateId": CLIENT_ME["AFF_ID"],
+    "format": "json",
+    "formatVersion": "2",
+    "keyword": "",
+    "hits": HITS_PER_PAGE,
+    "sort": "-itemPrice",
+    "page": 0,
+    "minPrice": 100,
+}
+WANT_ITEMS = [
+    "itemName",
+    "itemPrice",
+    "itemUrl",
+]
+
+path_output_dir = Path("./data/output/")
